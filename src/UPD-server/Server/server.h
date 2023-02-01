4dbe3e0f525258dce7c0ed602b673c296f9a18f8
Several_Camper <mikha.alkhimovich@mail.ru>
2023-02-01 23:39:18 +0700

    develop: finish first lab

---
+++ src/UPD-server/Server/server.h
@@ -0,0 +1,14 @@
+#ifndef SERVER_H_
+#define SERVER_H_
+
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#define BUF_SIZE 81
+
+#endif //  SERVER_H_
